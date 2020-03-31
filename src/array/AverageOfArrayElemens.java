5809a521bf87910d9a64b4809e058a98892db3bc
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-31 22:26:18 +0530

    add values to int array i.e int[] value and return it to an array defined in the main method  i.e anotherArray

--- src/array/AverageOfArrayElemens.java
+++ src/array/AverageOfArrayElemens.java
@@ -6,11 +6,22 @@ public class AverageOfArrayElemens {
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // int[] anotherArray = getIntegers(5);
+        int[] anotherArray = getIntegers(5);
 
     }
 
-//    public static int[] getIntegers(int numbers) {
-//
-//    }
+    public static int[] getIntegers(int numbers) {
+        System.out.println("Enter the number of integer value you would like to ad to your array i.e anotherArray");
+        //int [] values = scan.nextInt();  Not valid operation
+        //int values = scan.nextInt();     but this is valid
+
+        int[] values = new int[numbers];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scan.nextInt();
+
+        }
+        return values;
+
+    }
 }
