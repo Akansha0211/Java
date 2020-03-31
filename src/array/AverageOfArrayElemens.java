ff1ecf51eb21fac41bcda8e968552e5e12e63b0b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-31 22:32:56 +0530

    print the values in the array as returned by the getIntegers()method

--- src/array/AverageOfArrayElemens.java
+++ src/array/AverageOfArrayElemens.java
@@ -8,8 +8,19 @@ public class AverageOfArrayElemens {
     public static void main(String[] args) {
         int[] anotherArray = getIntegers(5);
 
+        // this will print the values of array elements returned by the getIntegers() method
+        for (int i = 0; i < anotherArray.length; i++) {
+            System.out.println("Element " + i + " typed value was" + anotherArray);
+        }
+
     }
 
+    /**
+     * This method will take input of Array elements from the user
+     *
+     * @param numbers the number of values an array has
+     * @return an array i.e int[] values to the array in the main method i.e int[] anotherArray
+     */
     public static int[] getIntegers(int numbers) {
         System.out.println("Enter the number of integer value you would like to ad to your array i.e anotherArray");
         //int [] values = scan.nextInt();  Not valid operation
