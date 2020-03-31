8d935e76369a6d34f0d17979945646ca3ae0ae15
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-31 22:16:52 +0530

    create Scanner object

--- src/array/AverageOfArrayElemens.java
+++ src/array/AverageOfArrayElemens.java
@@ -1,4 +1,16 @@
 package array;
 
+import java.util.Scanner;
+
 public class AverageOfArrayElemens {
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // int[] anotherArray = getIntegers(5);
+
+    }
+
+//    public static int[] getIntegers(int numbers) {
+//
+//    }
 }
