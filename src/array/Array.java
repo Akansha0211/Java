6d8a42f429e9e6814871ce89de17618b4932c3d6
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-31 21:29:50 +0530

    defining an Integer Array in a different way

---
+++ src/array/Array.java
@@ -0,0 +1,23 @@
+package array;
+
+public class Array {
+    public static void main(String[] args) {
+        // define an integer array
+        int[] intArray = new int[10];
+
+        //defining a double type Array
+        //double[] doubleArray = new double[10];
+
+        //Adding element to an array
+        intArray[0] = 20;
+
+        // defining and initializing an Array
+        int[] myIntArray = {1, 2, 3, 4, 5, 6};
+        System.out.println(myIntArray[0]);
+        System.out.println(myIntArray[1]);
+        System.out.println(myIntArray[2]);
+        System.out.println(myIntArray[3]);
+
+
+    }
+}
