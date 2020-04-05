a81766ca9fb8a15b1c8f01e17a2255e436c5ed77
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 03:55:27 +0530

    create getInput() method

--- src/ListAndArrayList/ResizeArray.java
+++ src/ListAndArrayList/ResizeArray.java
@@ -1,4 +1,22 @@
 package ListAndArrayList;
 
+import java.util.Scanner;
+
 public class ResizeArray {
+    private static Scanner scan = new Scanner(System.in);
+    private static int [] array = new int[10];
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * This method will give elements to the array from the console...
+     */
+    public static void getInput(){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+        }
+    }
+
 }
