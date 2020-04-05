06dccf9e0f9592988a658e2fb6f0fa6e924fdd3d
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 00:24:29 +0530

    declaring reference variable arr1 and arr2

--- src/referenceVsValueType/Main.java
+++ src/referenceVsValueType/Main.java
@@ -1,5 +1,7 @@
 package referenceVsValueType;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //primitive tye i.valueTypes storing values not references i.e objects (created by using new keyword)
@@ -11,5 +13,14 @@ public class Main {
         var2++; // on changing var2 which is totally independent of var1 it would be wrong to assume any change in var1 as well
         System.out.println("value of var1 = "+var1);
         System.out.println("value of var2 "+var2);
+
+        // declaring reference type...
+        int[] arr1 = new int[5];   // arr1---> |__|__|__|__|__|
+
+        int arr2[] = arr1;     // arr1---> |__|__|__|__|__|  <----- arr2
+                                // same array object is referenced to by two different objects...
+        System.out.println("arr1"+ Arrays.toString(arr1));
+        System.out.println("arr2"+Arrays.toString(arr2));
+
     }
 }
