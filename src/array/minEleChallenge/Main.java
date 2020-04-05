4c1e55555302e6fd71e493942b3392827b4961c7
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 02:13:57 +0530

    call the findMin() method in Main class

--- src/array/minEleChallenge/Main.java
+++ src/array/minEleChallenge/Main.java
@@ -3,6 +3,8 @@ package array.minEleChallenge;
 public class Main {
     public static void main(String[] args) {
         int [] arr = MinElementChallenge.readIntegers(5);
+        int minValueFromArray = MinElementChallenge.findMin(arr);
+        System.out.println(minValueFromArray);
 
     }
 }
