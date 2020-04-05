a9d6f0fb3a01a2f1a21b8588e48d5e69663d9063
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 00:39:21 +0530

    create modify() method

--- src/referenceVsValueType/Main.java
+++ src/referenceVsValueType/Main.java
@@ -23,9 +23,23 @@ public class Main {
         System.out.println("arr2"+Arrays.toString(arr2));
         arr2[0] = 1;
         System.out.println("After change in reference variable");
-        System.out.println("arr1"+ Arrays.toString(arr1));
-        System.out.println("arr2"+Arrays.toString(arr2));
+        System.out.println("arr1 = "+ Arrays.toString(arr1));
+        System.out.println("arr2 = "+Arrays.toString(arr2));
+        modify(arr1);
+        System.out.println("After modify method");
+        System.out.println("arr1 = "+ Arrays.toString(arr1));
+        System.out.println("arr2 = "+Arrays.toString(arr2));
+    }
 
+    /**
+     * This method wil modify index in the array which is specified as parameter
+     * It is 3rd reference to the array ...
+     * @param array change in it is passed to all the reference variables pointing the same array object...
+     */
+    private static void modify(int[]array){
+        array[0] = 2;
+        array = new int[]{1,2,3,4,5}; // de-referencing the array
+        array[1] = 3; // any further changes will not be reflected in any other arary
     }
 
 }
