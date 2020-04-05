0eb7e819e7db8ca6988e0a9ddb8928ce65613ed2
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 04:08:21 +0530

    create resizeArray() method

--- src/ListAndArrayList/ResizeArray.java
+++ src/ListAndArrayList/ResizeArray.java
@@ -18,10 +18,26 @@ public class ResizeArray {
             array[i] = scan.nextInt();
         }
     }
+
+    /**
+     * This method will print the array...
+     * @param arr
+     */
     public static void printArray(int[] arr){
         for (int i = 0; i < arr.length ; i++) {
             System.out.print(arr[i]+" ");
             System.out.println();
+        }
+    }
+
+    /**
+     * Ths method will resize the array
+     */
+    public static void resizeArray(){
+        int[] original = array;
+        array = new int[12];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = original[i];
         }
     }
 
