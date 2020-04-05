b07e6f51a72dec5e3f0d3f461d08207d1f2c9ac8
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 03:55:59 +0530

    create printArray() method

--- src/ListAndArrayList/ResizeArray.java
+++ src/ListAndArrayList/ResizeArray.java
@@ -17,6 +17,11 @@ public class ResizeArray {
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextInt();
         }
+    } public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+            System.out.println();
+        }
     }
 
 }
