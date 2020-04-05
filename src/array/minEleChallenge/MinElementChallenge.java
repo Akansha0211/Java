5834ebb29010200b1576a575e1891750719fce85
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-06 01:00:13 +0530

    create MinElementChallenge class

---
+++ src/array/minEleChallenge/MinElementChallenge.java
@@ -0,0 +1,17 @@
+package array.minEleChallenge;
+
+public class MinElementChallenge {
+    /**
+     * Write a method called readIntegers() with parameter called count that represents how may Integers the user needs to enter .
+     * The method needs to read from console until all the numbers are entered , and the return an array containing the numbers entered.
+     * Write a method findMin() with the array as a parameter. The method needs to return the minimum value in the array.
+     * In the main() method read the count from the console and call the method readIntegers() with the count parameter.
+     * Then call he findMin() method passing the array returned from the call to the readIntegers method.
+     * Finally ,print the minimum element in the array.
+     *
+     * Tips:
+     * Assume the user only enters numbers never letters.
+     *For simplicity,create a Scanner as a static field to help with data input.
+     * Create a new console project with the name 'MinElementChallenge'.
+     */
+}
