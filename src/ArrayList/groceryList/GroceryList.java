c9eb8705d1843a5beb5704f910e18b4a100af80f
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-18 23:24:12 +0530

    create printGroceryList() method which prints all the items in the GroceryList

--- src/ArrayList/groceryList/GroceryList.java
+++ src/ArrayList/groceryList/GroceryList.java
@@ -15,6 +15,20 @@ public class GroceryList {
         groceryList.add(item);
     }
 
+    /**
+     * This method will print the Grocery list items .
+     * uses (i+1) in print statement :
+     * as to the user the item number in the list begins with 1 ,... not 0 so [index(=0,....)+1]
+     */
+    public void printGroceryList(){
+        System.out.println("You have  "+ groceryList.size()+ "  items in the list ");
+        for (int i = 0; i <groceryList.size() ; i++) {
+            System.out.println((i+1)+"."+groceryList.get(i) );
+        }
+    }
+
+
+
 
 
 }
