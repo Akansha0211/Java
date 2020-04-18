d6d326849193e345068b00bb6f74355f0827a7c5
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-19 00:05:56 +0530

    prepare switch case

--- src/ArrayList/groceryList/Main.java
+++ src/ArrayList/groceryList/Main.java
@@ -1,5 +1,6 @@
 package ArrayList.groceryList;
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +8,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         GroceryList groceryLst = new GroceryList();
 
+        boolean quit = false;
+        int choice = 0;
+        //printInstructions();
+        while (!quit) {
+            System.out.println("Enter your choice ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (choice){
+                case 0:
+                    //printInstructions();
+                    break;
+                case 1:
+                    groceryLst.printGroceryList();
+                    break;
+                case 2:
+                    //addItem();
+                    break;
+                case 3:
+                    //modifyItem();
+                    break;
+                case 4:
+                    //removeItem();
+                    break;
+                case 5:
+                    //searchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+
+            }
+        }
     }
+
 }
