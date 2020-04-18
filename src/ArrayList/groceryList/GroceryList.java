cecc2d47eeb3d077833ff06bd7bf67c78cf4bc54
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-18 23:20:19 +0530

    create addGroceryList() method to the GroceryList class

--- src/ArrayList/groceryList/GroceryList.java
+++ src/ArrayList/groceryList/GroceryList.java
@@ -5,4 +5,16 @@ import java.util.ArrayList;
 public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<>();
 
+
+    /**
+     * This method will add an item to the grocery list.
+     * uses add() method of the ArrayList class
+     * @param item is to be added to the List...
+     */
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+    }
+
+
+
 }
