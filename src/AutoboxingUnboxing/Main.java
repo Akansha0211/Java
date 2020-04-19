cd6445dcfdf153aaa73d812551af987948273f6b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-19 23:46:42 +0530

    creating Integer ArrayList and Double ArrayList

--- src/AutoboxingUnboxing/Main.java
+++ src/AutoboxingUnboxing/Main.java
@@ -28,5 +28,38 @@ public class Main {
 
         ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
         intClassArrayList.add(new IntClass(54));
+
+        Integer integer = new Integer(23);
+        Double doubleValue = new Double(25.7);
+        System.out.println(integer);
+        System.out.println(doubleValue);
+
+        ArrayList<Integer> myArrayList = new ArrayList<Integer>();
+        for (int i = 0; i <= 10 ; i++) {
+            myArrayList.add(Integer.valueOf(i));
+        }
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(i+" -->"+ myArrayList.get(i).intValue());
+        }
+
+
+        //Autoboxing....
+        Integer myIntValue = Integer.valueOf(56);
+        System.out.println(myIntValue);
+        Integer myIntvalu = 56;
+        System.out.println(myIntvalu);
+
+        //Unboxing....
+        int myInt = myIntValue.intValue();
+        System.out.println(myInt);
+
+        ArrayList<Double> doubleValues = new ArrayList<Double>();
+        for (double dbl = 0; dbl <=10 ; dbl+=0.5) {
+            doubleValues.add(Double.valueOf(dbl));
+        }
+        for (int i = 0; i<= doubleValues.size();i++){
+            double values = doubleValues.get(i).intValue();
+            System.out.println(i+ " -->"+ values);
+        }
     }
 }
