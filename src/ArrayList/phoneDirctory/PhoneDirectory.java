222a2f584a936d41fa3e62cf61bd784be5743655
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-19 16:04:21 +0530

    create findContact() method

--- src/ArrayList/phoneDirctory/PhoneDirectory.java
+++ src/ArrayList/phoneDirctory/PhoneDirectory.java
@@ -11,5 +11,25 @@ public class PhoneDirectory {
         this.myContacts = new ArrayList<Contact>();
     }
 
+//    public boolean addNewContact(Contact contact){
+//        if (findContact(contact)>= 0) {
+//            System.out.println("Contact is already on the file ");
+//            return false;
+//        }
+//
+//        myContacts.add(contact);
+//        return true;
+//    }
+
+
+    /**
+     * Tgis method will find that a particular contact is n the the  PhoneDirectory
+     * IF present than return index greater than or equal to zero otherwise return -1
+     * @param contact which is to be searched
+     * @return the index...
+     */
+    public int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
 
 }
