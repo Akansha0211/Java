234d0efe7824c6516db82b322d6133f2b898e9ea
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-22 00:22:48 +0530

    create Constructor for the Customer class

--- src/AutoboxingUnboxing/BankingApplication/Customer.java
+++ src/AutoboxingUnboxing/BankingApplication/Customer.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 public class Customer {
     private String name;
     private ArrayList<Double> transactions;
-
+    public Customer(String name, double initialAmount){
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+    }
 
 }
