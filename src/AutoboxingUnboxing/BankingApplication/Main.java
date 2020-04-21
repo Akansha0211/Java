39297553b1e9657957d17c76d12d34572b1e9d5f
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-22 00:13:52 +0530

    create a Main class in BankingApplication package

---
+++ src/AutoboxingUnboxing/BankingApplication/Main.java
@@ -0,0 +1,22 @@
+package AutoboxingUnboxing.BankingApplication;
+
+public class Main {
+    // Your job is to create a simple banking application .
+    // There should be a Bank class
+    //It should have an arrayList of Branches
+    //Each Branch should have an arrayList of Customers.
+    //The Customer class should have an arrayList of Doubles(Transactions)
+    //Customers :
+    //Name to be able to add a new customer and initial transaction amount
+    // Also needs to add additional transactions for that customer/branch
+    //Bank:
+    //Add a new branch
+    //Add a customer to that branch with initial transaction
+    //Add a transaction for an existing customer for that branch
+    // Show a list of customers for a particular branch and optionally a list o their transactions
+    //Demonstration autoboxing and unboxing in your code
+    // Hint : Transactions
+    //Add data validation
+    // e.g. check if exists ,or does not exists ,etc.
+    //Think about where you are adding the code to perform certain actions.
+}
--- src/AutoboxingUnboxing/ChallengeExercise/Main.java
+++
@@ -1,4 +0,0 @@
-package AutoboxingUnboxing.ChallengeExercise;
-
-public class Main {
-}
