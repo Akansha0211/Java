969f42caed537e70d81df64487963225b2cd27bc
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-22 00:32:46 +0530

    create Constructor for the Branch class

--- src/AutoboxingUnboxing/BankingApplication/Branch.java
+++ src/AutoboxingUnboxing/BankingApplication/Branch.java
@@ -5,5 +5,8 @@ import java.util.ArrayList;
 public class Branch {
     private String name;
     private ArrayList<Customer> customers;
-
+    public Branch(String name){
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
 }
