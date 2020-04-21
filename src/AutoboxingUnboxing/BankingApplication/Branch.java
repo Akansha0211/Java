784fcd043ee1f1c34e8029907f8cb847bcc5051f
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-22 00:48:41 +0530

    create findCustomer() method in Branch class

--- src/AutoboxingUnboxing/BankingApplication/Branch.java
+++ src/AutoboxingUnboxing/BankingApplication/Branch.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Branch {
     private String name;
-    private ArrayList<Customer> customers;
+    private ArrayList<Customer> customers; // Customer Array List
     public Branch(String name){
         this.name = name;
-        this.customers = new ArrayList<Customer>();
+        this.customers = new ArrayList<Customer>(); // initializing CustomerArray List
     }
     public String getName(){
         return name;
@@ -15,4 +15,26 @@ public class Branch {
     public ArrayList<Customer> getCustomers(){
         return customers;
     }
+//    public boolean addCustomers(String customerName, double initialAmount){
+//        if (findCustomer(customerName)== null) {
+//            this.customers.add(new Customer(customerName,initialAmount));
+//            return true;
+//        }
+//        return false;
+//    }
+
+    /**
+     * This method will find whether a customer with particular name is present in the customer Array list of Branch or not
+     * @param customerName Customer with this particular name in the Array List is to be searched for
+     * @return Customer
+     */
+    public Customer findCustomer(String customerName){
+        for (int i = 0; i < customers.size(); i++) {
+            if (this.customers.get(i).getName()== customerName){
+                return this.customers.get(i);
+            }
+        }
+        return null;
+    }
+
 }
