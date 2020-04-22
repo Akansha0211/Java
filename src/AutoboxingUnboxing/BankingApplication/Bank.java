d519bc325996ea706815254079eef03d220da271
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-22 10:25:26 +0530

    create data-fields for Bank class

--- src/AutoboxingUnboxing/BankingApplication/Bank.java
+++ src/AutoboxingUnboxing/BankingApplication/Bank.java
@@ -1,4 +1,12 @@
 package AutoboxingUnboxing.BankingApplication;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private String name; // Bank name
+    private ArrayList<Branch> branches; // ArrayList for branches
+
+
+
+
 }
