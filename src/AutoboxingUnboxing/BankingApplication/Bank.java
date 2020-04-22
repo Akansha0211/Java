91ebcc3626310c58678b0b6fce0524788c07991e
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-22 10:27:18 +0530

    create Constructor for Bank class

--- src/AutoboxingUnboxing/BankingApplication/Bank.java
+++ src/AutoboxingUnboxing/BankingApplication/Bank.java
@@ -6,7 +6,10 @@ public class Bank {
     private String name; // Bank name
     private ArrayList<Branch> branches; // ArrayList for branches
 
-
+    public Bank(String name){
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
 
 
 }
