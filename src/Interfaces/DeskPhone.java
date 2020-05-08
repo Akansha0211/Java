4b74215eef0cdfb06d9f503d2137678714d8d4c8
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-08 23:20:11 +0530

    DeskPhone implements iTelephone interface

--- src/Interfaces/DeskPhone.java
+++ src/Interfaces/DeskPhone.java
@@ -1,4 +1,28 @@
 package Interfaces;
 
-public class DeskPhone {
+public class DeskPhone implements iTelephone {
+    @Override
+    public void powerOn() {
+
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+
+    }
+
+    @Override
+    public void answer() {
+
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        return false;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return false;
+    }
 }
