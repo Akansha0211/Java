928df4dcdfc954d02f063f4f8e6fbe755a102e35
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-08 23:36:01 +0530

    create instance for the DeskPhone class

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -1,4 +1,7 @@
 package Interfaces;
 
 public class Main {
+    public static void main(String[] args) {
+        iTelephone phone = new DeskPhone(1234567); // create instance for DeskPhone class
+    }
 }
