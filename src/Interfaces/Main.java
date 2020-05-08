acf96cae9a4f8b5b7c8ef96acfcf36d5a7e398d7
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-08 23:45:15 +0530

    call methods in Desktop Class to main method

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -3,5 +3,13 @@ package Interfaces;
 public class Main {
     public static void main(String[] args) {
         iTelephone phone = new DeskPhone(1234567); // create instance for DeskPhone class
+
+        // can't instantiate Interface
+       // iTelephone phone1 = new iTelephone();
+
+       phone.powerOn();
+       phone.callPhone(1234567);
+       phone.answer();
+
     }
 }
