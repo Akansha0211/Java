bd95c8be8e199fb90c8e721c93a61c49a19fa2f6
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-08 23:18:35 +0530

    complete interface

--- src/Interfaces/iTelephone.java
+++ src/Interfaces/iTelephone.java
@@ -1,4 +1,19 @@
 package Interfaces;
 
+/**
+ * Creating an interface
+ * Interface is like a contract that the method signatures defined in the interface will not change.
+ * A class that implements the interface implements  all the methods of the interface
+ * with exact signature as that present in the interface
+ * Interface is Abstract Data Type i.e it consists of no code
+ * Use : Provide a common behaviour to  several classes implementing it
+ * Access modifiers can be omitted as it will be included in the actual class...
+ */
 public interface iTelephone {
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+
 }
