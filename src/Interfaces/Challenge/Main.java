81c26a3272803964075052231d1a52697ee09923
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-11 09:54:51 +0530

    create readValues() method

--- src/Interfaces/Challenge/Main.java
+++ src/Interfaces/Challenge/Main.java
@@ -1,5 +1,8 @@
 package Interfaces.Challenge;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /**
@@ -12,7 +15,7 @@ public class Main {
          * Create a few sample classes that implement  your Saveable interface (we've used the idea of the game
          * with Players and Monsters but you write any type of classes that you want) .
          *
-         * Override the toString() method for each for each of your classes so that they can be easily printed to enable the
+         * Override the toString() method for each of your classes so that they can be easily printed to enable the
          * program to be tested easily.
          *
          * In the main method write a method that takes object that implements the interface as a parameter and saves the
@@ -26,5 +29,36 @@ public class Main {
          * from the file - this allows you to type as many values as your class requires , and returns an ArrayList .
          * There is a whole Java section to be learnt where we will be using files, etc...
          */
+    }
+
+    /**
+     * This method will input a String
+     * @return Arraylist of values to be saved ....
+     */
+    public static ArrayList<String> readValues(){
+        ArrayList<String> values = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false ;
+        int index = 0;
+        System.out.println("Choose \n" +
+                "1 to enter a String \n" +
+                "0 to quit ");
+        while (!quit){
+            System.out.println("Enter your choice as stated above ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a String");
+                    String stringInput = scanner.nextLine();
+                    values.add(index,stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
     }
 }
