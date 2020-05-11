eb0b435a7bbf899eb19fd66c97775aa29242e3b3
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-11 10:12:14 +0530

    implement methods of iSaveable interface in Player class

--- src/Interfaces/Challenge/Player.java
+++ src/Interfaces/Challenge/Player.java
@@ -1,4 +1,17 @@
 package Interfaces.Challenge;
 
-public class Player {
+import java.util.ArrayList;
+
+public class Player implements iSaveable {
+
+
+    @Override
+    public ArrayList<String> write() {
+        return null;
+    }
+
+    @Override
+    public void read(ArrayList<String> savedValues) {
+
+    }
 }
