53318e2821bb2294ef0e051f04511d31e9e71af6
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-11 10:06:19 +0530

    method declaration in iSaveable interface

--- src/Interfaces/Challenge/iSaveable.java
+++ src/Interfaces/Challenge/iSaveable.java
@@ -1,4 +1,16 @@
 package Interfaces.Challenge;
 
+import java.util.ArrayList;
+
+/**
+ * Create a single interface  ( name : Saveable)
+ *  That interface allows an object to be saved to some sort of storage medium (use ArrayList)
+ *  The exact type of medium is not known to the interface (nor to the classes that implement it)
+ *
+ *  The interface will just specify two methods , one to return an ArrayList of values to be saved
+ *   and other to populate the object's fields from an ArrayList(parameter)...
+ */
 public interface iSaveable {
+    ArrayList<String> write();
+    void read(ArrayList<String> savedValues);
 }
