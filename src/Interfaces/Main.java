d20498fd156c9b1551048ef547855b18aa30f893
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-11 09:08:16 +0530

    create Main class

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -2,13 +2,19 @@ package Interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        iTelephone phone = new DeskPhone(1234567); // create instance for DeskPhone class
+        iTelephone phone ;
+        phone= new DeskPhone(1234567); // create instance for DeskPhone class
 
         // can't instantiate Interface
        // iTelephone phone1 = new iTelephone();
 
        phone.powerOn();
        phone.callPhone(1234567);
+       phone.answer();
+
+       phone = new MobilePhone(243565);
+       phone.powerOn();
+       phone.callPhone(243565);
        phone.answer();
 
     }
