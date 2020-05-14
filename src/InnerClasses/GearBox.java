d0c8125ac1030c712ec18ac78eb28be74b2debe8
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-14 23:21:54 +0530

    create Gear class

--- src/InnerClasses/GearBox.java
+++ src/InnerClasses/GearBox.java
@@ -1,4 +1,33 @@
 package InnerClasses;
 
+import java.util.ArrayList;
+
 public class GearBox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int gearNumber;
+
+    public GearBox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<Gear>();
+        // TO initialise ArrayList gear
+
+        Gear neutral =  new Gear(0,0.0);
+        this.gears.add(neutral);
+
+    }
+
+    public class Gear{
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double driveSpeed(int revs){
+            return revs *(this.ratio);
+        }
+    }
 }
