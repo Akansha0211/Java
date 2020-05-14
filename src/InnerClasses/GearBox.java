42f2e1b7a7c17807337683de47a3523d898a14d5
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-14 23:49:55 +0530

    change in the GearBox class

--- src/InnerClasses/GearBox.java
+++ src/InnerClasses/GearBox.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class GearBox {
     private ArrayList<Gear> gears;
     private int maxGears;
-    private int gearNumber;
+    private int gearNumber = 0;
 
     public GearBox(int maxGears) {
         this.maxGears = maxGears;
