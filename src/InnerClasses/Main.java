43233519cf1ec5c11b63d2a16350d200a4372ee2
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-14 23:54:38 +0530

    create instance of Gear class

--- src/InnerClasses/Main.java
+++ src/InnerClasses/Main.java
@@ -1,4 +1,9 @@
 package InnerClasses;
 
 public class Main {
+    public static void main(String[] args) {
+        GearBox mcLaren = new GearBox(6);
+        GearBox.Gear first = mcLaren.new Gear(1,12.3);
+        System.out.println(first.driveSpeed(1000));
+    }
 }
