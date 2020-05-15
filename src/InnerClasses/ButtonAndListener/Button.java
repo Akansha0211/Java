410d2457dc4a1c8f93495f5895bcb2f1690077bb
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-15 13:18:31 +0530

    create OnClickListener interface  in Button class

--- src/InnerClasses/ButtonAndListener/Button.java
+++ src/InnerClasses/ButtonAndListener/Button.java
@@ -1,4 +1,34 @@
 package InnerClasses.ButtonAndListener;
 
 public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Ths function is to set onClickListner
+     * It will handle the clicks
+     * @param onClickListener  it will handle he clicks...
+     */
+   public void setOnClickListener(OnClickListener onClickListener){
+       this.onClickListener = onClickListener;
+   }
+
+    /**
+     * Button will set onClickListener() method which were created to assign an object to Button in order to handle clicks
+     */
+   public void onClick(){
+       this.onClickListener.onClick(this.title);
+   }
+   public interface OnClickListener{
+       public void onClick(String title);
+   }
+
 }
