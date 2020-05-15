778c9f9de0d98cd14d8999c3b36bce9a8ded95cd
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-15 13:23:00 +0530

    create object of Scanner and Button class

--- src/InnerClasses/ButtonAndListener/Main.java
+++ src/InnerClasses/ButtonAndListener/Main.java
@@ -1,6 +1,10 @@
 package InnerClasses.ButtonAndListener;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner  scanner= new Scanner(System.in);
+    private static  Button btnPrint = new Button("Print");
     public static void main(String[] args) {
         /**
          * Create your button class and attach a listener to it
