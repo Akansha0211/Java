094a077dd20d457ad123681c6dca91b56e3f4e7d
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-15 11:20:37 +0530

    create Main class in ButtonAndListener interface

---
+++ src/InnerClasses/ButtonAndListener/Main.java
@@ -0,0 +1,10 @@
+package InnerClasses.ButtonAndListener;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Create your button class and attach a listener to it
+         * Demonstrate Local classes....
+         */
+    }
+}
