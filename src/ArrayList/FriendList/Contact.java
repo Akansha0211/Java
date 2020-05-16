d0b671b0f82da634028a19606c5eee43649c8292
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-16 08:55:25 +0530

    create constructor for Contact class

--- src/ArrayList/FriendList/Contact.java
+++ src/ArrayList/FriendList/Contact.java
@@ -1,4 +1,13 @@
 package ArrayList.FriendList;
 
 public class Contact {
+    private String name;
+    private  int mobileNumber;
+
+    public Contact(String name, int mobileNumber) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+    }
+
+
 }
