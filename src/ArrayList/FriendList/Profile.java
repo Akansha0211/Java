c65eb43491aeb55c3003fa7edbae7eab90e94a3c
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-16 08:53:55 +0530

    create Contact class

--- src/ArrayList/FriendList/Profile.java
+++ src/ArrayList/FriendList/Profile.java
@@ -1,4 +1,10 @@
 package ArrayList.FriendList;
 
+import array.Array;
+
+import java.util.ArrayList;
+
 public class Profile {
+    private String name;
+    private ArrayList<Contact> friendList;
 }
