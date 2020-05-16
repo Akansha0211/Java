a63cb35eaefd0b55fd961c6d3f65eecd16bc9346
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-16 08:57:20 +0530

    craeate object of FriendList in constructor of Profile class

--- src/ArrayList/FriendList/Profile.java
+++ src/ArrayList/FriendList/Profile.java
@@ -7,4 +7,8 @@ import java.util.ArrayList;
 public class Profile {
     private String name;
     private ArrayList<Contact> friendList;
+
+    public Profile(ArrayList<Contact> friendList) {
+        this.friendList = new ArrayList<>();
+    }
 }
