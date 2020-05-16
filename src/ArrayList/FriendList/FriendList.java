7e292bbb170ffb70fc82cc3880ebfb590f4a1ab0
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-16 09:02:16 +0530

    create a constructor for FriendList class

--- src/ArrayList/FriendList/FriendList.java
+++ src/ArrayList/FriendList/FriendList.java
@@ -1,6 +1,15 @@
 package ArrayList.FriendList;
 
+import java.util.ArrayList;
+
 public class FriendList implements iFriendList {
+
+    private ArrayList<Contact> friendList;
+
+    public FriendList(ArrayList<Contact> friendList) {
+        this.friendList = new ArrayList<>();
+    }
+
     @Override
     public boolean acceptFriendRequest() {
         return false;
