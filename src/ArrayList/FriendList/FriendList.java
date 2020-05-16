791174ea61e5fe300a8bffa436913bbd7d1c2d10
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-16 08:45:47 +0530

    FriendList class implements iFriendList interface

--- src/ArrayList/FriendList/FriendList.java
+++ src/ArrayList/FriendList/FriendList.java
@@ -1,4 +1,28 @@
 package ArrayList.FriendList;
 
-public class FriendList {
+public class FriendList implements iFriendList {
+    @Override
+    public boolean acceptFriendRequest() {
+        return false;
+    }
+
+    @Override
+    public void removeFromFriendList() {
+
+    }
+
+    @Override
+    public void modifyMyProfile() {
+
+    }
+
+    @Override
+    public void viewFriendList() {
+
+    }
+
+    @Override
+    public boolean findFriendInList() {
+        return false;
+    }
 }
