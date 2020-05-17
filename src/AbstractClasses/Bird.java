252d3a99d3090bfcf66e390b18c9a5cd1a770017
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-17 12:16:09 +0530

    implement method canFly() from CanFly interface

--- src/AbstractClasses/Bird.java
+++ src/AbstractClasses/Bird.java
@@ -2,7 +2,7 @@ package AbstractClasses;
 
 //Now making Bird class also Abstract
 // abstract class itself that inherits from another abstract class
-public abstract class Bird extends Animal {
+public abstract class Bird extends Animal implements CanFly{
     public Bird(String name) {
         super(name);
     }
@@ -17,5 +17,11 @@ public abstract class Bird extends Animal {
         System.out.println("Breath in breath out , repeat");
     }
 
-    public abstract void fly(); // creating an Abstract method that individual Bird objects could occupy
+//    public abstract void fly(); // creating an Abstract method that individual Bird objects could occupy
+
+
+    @Override
+    public void canFly() {
+        System.out.println(getName()+" if Flapping its wheel");
+    }
 }
