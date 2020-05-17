3747e6a253e79380e2a3d5559c63538ca1e2c408
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-17 11:23:14 +0530

    comments added to the abstract methods

--- src/AbstractClasses/Animal.java
+++ src/AbstractClasses/Animal.java
@@ -6,7 +6,8 @@ public abstract class Animal {
     public Animal(String name) {
         this.name = name;
     }
-
+    //By creating abstract methods we are forcing the class which is inheriting the Abstract class to implement all of its methods
+    //Where as while normally a class we don't need to implement all of its methods...
     public abstract void eat();
     public abstract void breath();
 
