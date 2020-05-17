756810dad3cf86af5a1247a1df7a41f9f77ff0ff
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-17 11:15:19 +0530

    create an Abstract class Animal

--- src/AbstractClasses/Main.java
+++ src/AbstractClasses/Main.java
@@ -1,4 +1,13 @@
 package AbstractClasses;
 
 public class Main {
+
+    //Abstraction is when you define the functionality without implementing
+    //Focused on what needs to be done rather than how it is to be done
+    //Interfaces are abstract and actual implementation are left to teh classes that implement the interfaces
+    //Interfaces can't be instantiated . Classes that implements the interfaces can be instantiated...
+
+
+    //Java also provide abstract classes and these are the methods that provide definition but no implementation  to those methods
+    //Implementation is left to the class that inherits an abstract class.
 }
