8e739cc29d393413cb7c55dfc89781c995586eb1
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-17 11:17:43 +0530

    create Constructor , methods and getter to this abstract class Animal

--- src/AbstractClasses/Animal.java
+++ src/AbstractClasses/Animal.java
@@ -1,4 +1,16 @@
 package AbstractClasses;
 
-public class Animal {
+public abstract class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void eat();
+    public abstract void breath();
+
+    public String getName() {
+        return name;
+    }
 }
