ad225dcef56107528dbf7ce48121af63dff9c30f
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-21 13:20:09 +0530

    create object of Team and inherited classes of Player class

--- src/Generics/OurGenericClass/Main.java
+++ src/Generics/OurGenericClass/Main.java
@@ -1,4 +1,19 @@
 package Generics.OurGenericClass;
 
 public class Main {
+    public static void main(String[] args) {
+        FootballPlayer ayush = new FootballPlayer("ayush");
+        BaseballPlayer panshul = new BaseballPlayer("panshul");
+        SoccerPlayer mukund = new SoccerPlayer("mukund");
+
+        Team footballTeam = new Team("FootballTeam");
+        footballTeam.addPlayer(ayush);
+        footballTeam.addPlayer(panshul);
+        footballTeam.addPlayer(mukund);
+        System.out.println(footballTeam.numOfPlayers());
+        //Above we see that ayush , panshul , mukund all of them are not football players only ayush is a football player
+
+        // so we need to make Team class Generics as well.
+    }
+
 }
