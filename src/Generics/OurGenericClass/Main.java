a966c2f10dfcdaf2a8c9463ecf3c4a99164f44ee
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-21 13:47:41 +0530

    create Team object of type String type

--- src/Generics/OurGenericClass/Main.java
+++ src/Generics/OurGenericClass/Main.java
@@ -24,6 +24,12 @@ public class Main {
 //        baseballTeam.addPlayer(mukund); // now this will give us an error as Now Type Of Team is BaseballPlayer.Earlier it was not the case..
         baseballTeam.addPlayer(panshul);
 
+        Team<String> brokenTeam = new Team<>("This should not have woorked"); // this should give error but not giving right now
+        //This means you can add any type to Team..
+        brokenTeam.addPlayer("no one");
+        // To sort the above problem type parameter of Team will extend  Player class so that subtype of Player i.e FootballPlayer,BaseballPlayer,SoccerPlayer
+        //Not any String...
+
 
 
 
