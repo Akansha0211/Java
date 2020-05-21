930a4d2aa3d8eb297627b273f6620e039fdc2944
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-21 11:37:02 +0530

    create addPlayer() method

--- src/Generics/OurGenericClass/Team.java
+++ src/Generics/OurGenericClass/Team.java
@@ -19,4 +19,19 @@ public class Team {
         return name;
     }
 
+    /**
+     * This method will return wherther a player acn be added ito the ArrayList  or not
+     * @param player
+     * @return true if a palyer acan be added or otherwise false
+     */
+    public boolean addPlayer(Player player){
+        if (members.contains(player)){
+            System.out.println(player.getName()+" is already in the team");
+            return false;
+        }else {
+            members.add(player);
+            System.out.println(player.getName()+" picked for the team "+this.name);  //this.name is the actual team name
+            return true;
+        }
+    }
 }
