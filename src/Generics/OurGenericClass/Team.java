313a96b2af51b3861375822258a2951f5741f487
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-21 11:31:05 +0530

    create constructor and getter for name of Team

--- src/Generics/OurGenericClass/Team.java
+++ src/Generics/OurGenericClass/Team.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 public class Team {
     private String name;
 
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
 
+    private ArrayList<Player> members = new ArrayList<>();
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
