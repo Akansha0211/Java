2cae4a0a77a0d61ebf30aacd6768eeed8b218602
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-23 14:54:30 +0530

    create LinkedList object

--- src/LinkedList/Demo.java
+++ src/LinkedList/Demo.java
@@ -1,4 +1,10 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<>();
+    }
+
 }
