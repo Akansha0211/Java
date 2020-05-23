97c369df5471983998da35affd9ecadae1b8cf93
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-23 14:58:16 +0530

    add values to the LinkedList of String type

--- src/LinkedList/Demo.java
+++ src/LinkedList/Demo.java
@@ -1,10 +1,24 @@
 package LinkedList;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+        //printList(placesToVisit);
+
     }
+//    private static void printList(LinkedList<String> linkedList){
+//
+//    }
 
 }
