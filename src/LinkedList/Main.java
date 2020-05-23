af9be993881c4c9ae2b00f4c1850467ed901733b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-23 14:05:45 +0530

    create objects for Customer class and anotherCustomer object is setting setBalance() for customer object

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,4 +1,11 @@
 package LinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Akansha",54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(32.5);
+        System.out.println("Balance for customer "+customer.getName()+" is "+customer.getBalance());
+    }
 }
