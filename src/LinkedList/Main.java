ae6aa679ef81fa18646b85666d623d949aecbe89
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-23 14:10:06 +0530

    create an ArrayList an add integer values to it

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,11 +1,23 @@
 package LinkedList;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer("Akansha",54.96);
         Customer anotherCustomer;
-        anotherCustomer = customer;
+        anotherCustomer = customer; // both the reference points to the same objects....
         anotherCustomer.setBalance(32.5);
         System.out.println("Balance for customer "+customer.getName()+" is "+customer.getBalance());
+
+
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);// autoboxing is done here
+        items.add(2);
+        items.add(3);
+
+        for (int i = 0; i < items.size(); i++) {
+            System.out.println(i+ ":"+ items.get(i));
+        }
     }
 }
