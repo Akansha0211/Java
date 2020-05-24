38616a228c31c6fdd338f8eb433432a69453c9d3
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-24 12:45:09 +0530

    create Seat class i.e inner class for Theatre class

--- src/Collections/Theatre.java
+++ src/Collections/Theatre.java
@@ -1,4 +1,39 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Theatre {
+    private final String theatreName;
+    private List<Seat> seats = new ArrayList<>();
+
+    public Theatre(String theatreName, int numRows, int seatPerRows){
+        this.theatreName = theatreName;
+        int lastRow = 'A'+(numRows-1);
+        for (char row ='A'; row<= lastRow;row++){
+            for (int seatNum =1 ; seatNum<=seatPerRows;seatNum++){
+                Seat seat = new Seat(row + String.format("%02d",seatNum));
+            }
+        }
+    }
+    public String getTheatreName(){
+        return theatreName;
+    }
+    public List<Seat> getSeat(){
+        return seats;
+    }
+
+//    public boolean reserveSeat(String seatNumber){
+//        Seat requestedSeat
+//    }
+
+    private class Seat{
+        private String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber){
+            this.seatNumber = seatNumber;
+        }
+    }
+
 }
