98c2cc375abf490cf296e2516618d58d80ae81bf
Akansha0211 <sanjaysxn25@gmail.com>
2020-06-21 23:12:17 +0530

    complete challenge1

--- src/Regex/Challenges.java
+++ src/Regex/Challenges.java
@@ -1,4 +1,12 @@
 package Regex;
 
 public class Challenges {
+    public static void main(String[] args) {
+        /**
+         * Write the string literal regex that will match the following String.
+         * use the String.matches() method to verify your answer.
+         */
+        String challenge1 ="I want a bike";
+        System.out.println((challenge1.matches("I want a bike")));
+    }
 }
