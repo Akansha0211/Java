6d34f633eb17397592999e56dc3e0ca4b7c298f5
Akansha0211 <sanjaysxn25@gmail.com>
2020-06-27 12:51:29 +0530

    craete Constructor in Theatre class and also create Seat class as inner class

--- src/Collections/Theatre.java
+++ src/Collections/Theatre.java
@@ -1,4 +1,30 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Theatre {
+    String theatreName;
+    List<Seat> seats = new ArrayList<>();
+    public Theatre(String theatreName, int numRows , int seatPerRow){
+        this.theatreName = theatreName;
+        int lastRow = 'A'+(numRows-1);
+        for (char row ='A'; row<=lastRow; row++){
+            for (int seatNum = 1; seatNum<= seatPerRow;seatNum++){
+                Seat seat = new Seat(row + String.format("%02d",seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+    public String getTheatreName(){
+        return theatreName;
+    }
+
+    public  class Seat{
+        String seatNum;
+        boolean reserved= false;
+        public Seat(String seatNum){
+            this.seatNum = seatNum;
+        }
+    }
 }
