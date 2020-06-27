90586df82d22d8c53cb0db47931329e99dc7de52
Akansha0211 <sanjaysxn25@gmail.com>
2020-06-27 14:05:13 +0530

    Main class complete

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -13,7 +13,10 @@ public class Main {
 //        }else {
 //            System.out.println("Sorry, seat is taken");
 //        }
-
+        /**
+         * Creating shallow copy of theatre.seats
+         * Only reference differs but both reference to the same objects
+         */
         List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);  // shallow copy of t
         printList(seatCopy);
         seatCopy.get(1).reserve(); // this will affect seatCopy and theater.seats both as same elements with different reference variable
@@ -30,10 +33,31 @@ public class Main {
         //System.out.println(theatre.seats);
         printList(theatre.seats);
 
+//        List<Theatre.Seat>newList= new ArrayList<>(theatre.seats.size());
+//        Collections.copy(newList,theatre.seats);
+//        System.out.println("Printing newList");
+//        printList(newList);
+
+        System.out.println("\n");
+        System.out.println("**********************");
+        Theatre.Seat minSeat = Collections.min(seatCopy);
+        Theatre.Seat maxSeat = Collections.max(seatCopy);
+        System.out.println("Min seat number is "+minSeat.getSeatNum());
+        System.out.println("Max seat number is"+maxSeat.getSeatNum());
+
     }
     public static void printList(List<Theatre.Seat>list){
         for (Theatre.Seat seat : list){
             System.out.print(" "+seat.getSeatNum());
+        }
+    }
+    public static void sortList(List<? extends Theatre.Seat>list){
+        for (int i = 0;i<list.size()-1;i++){
+            for (int j = 0; j<list.size()-1;j++){
+                if (list.get(i).compareTo(list.get(j))>0){
+                    Collections.swap(list,i,j);
+                }
+            }
         }
     }
 }
